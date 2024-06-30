62bfc3e6ec75c60c1d571bb91da5a333919d86e0
Prosk8er <prosk8er@gotskillslounge.com>
2024-06-30 19:47:47 -0400

    remove for now

--- ProsilverSlideEdition/template/mySidebarpsl.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidebarpsl").style.width = "250px";
-  document.getElementById("mainpsl").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebarpsl").style.width = "0";
-  document.getElementById("mainpsl").style.marginLeft= "0";
-}
